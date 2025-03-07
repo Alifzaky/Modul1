ea4fa696accffe9ed54901efb6bfdc0ae9a33f8b
pp <nasywa.muhammad70@gmail.com>
2025-03-07 08:50:11 +0700

    update 1

---
+++ codelabTugas/Modul_1/src/coba.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class coba {
+    public static void main(String[] args) {
+
+        String nama;
+        Scanner namaa = new Scanner(System.in);
+
+        System.out.print("Masukkan nama: ");
+        nama = namaa.next();
+
+        System.out.println("Nama kamu adalah : "  + nama );
+
+    }
+}
